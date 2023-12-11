b64e93cd08ba3a63c702b10189f46e5446dad1c5
Saivikaspan <112777887+Saivikaspan@users.noreply.github.com>
2023-12-12 00:37:42 +0530

    Update index.js

--- http-server/index.js
+++ http-server/index.js
@@ -1,1 +1,79 @@
-console.log("Hello GitHub!");
+const http = require("http");
+const fs = require("fs");
+const args = require("minimist")(process.argv.slice(2));
+
+let registrationContent = "";
+let homeContent = "";
+let projectContent = "";
+let cssContent = "";
+let jsContent = "";
+
+fs.readFile("registration.html", (err, data) => {
+  if (err) {
+    throw err;
+  }
+  registrationContent = data;
+});
+
+fs.readFile("home.html", (err, data) => {
+  if (err) {
+    throw err;
+  }
+  homeContent = data;
+});
+
+fs.readFile("project.html", (err, data) => {
+  if (err) {
+    throw err;
+  }
+  projectContent = data;
+});
+
+fs.readFile("index.css", (err, data) => {
+  if (err) {
+    throw err;
+  }
+  cssContent = data;
+});
+
+fs.readFile("index.js", (err, data) => {
+  if (err) {
+    throw err;
+  }
+  jsContent = data;
+});
+
+const port = args.port || 5000;
+
+http.createServer((req, res) => {
+  let url = req.url;
+  if (url === "/index.css") {
+    res.writeHead(200, { "Content-Type": "text/css" });
+    res.write(cssContent);
+    res.end();
+    return;
+  } else if (url === "/index.js") {
+    res.writeHead(200, { "Content-Type": "text/javascript" });
+    res.write(jsContent);
+    res.end();
+    return;
+  }
+
+  res.writeHead(200, { "Content-Type": "text/html" });
+
+  switch (url) {
+    case "/registration":
+      res.write(registrationContent);
+      break;
+    case "/project":
+      res.write(projectContent);
+      break;
+    default:
+      res.write(homeContent);
+      break;
+  }
+
+  res.end();
+}).listen(port, () => {
+  console.log(Server is running on port ${port});
+});
